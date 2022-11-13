06fe1aeaefd64817d58312109e982a553fa3241c
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-13 02:34:26 +0100

    feat(e_commerce/utils): adds functions to operate on models, and file storage api class

--- e_commerce/utils.py
+++ e_commerce/utils.py
@@ -1,7 +1,13 @@
 import uuid
 
+import requests
+from django.conf import settings
 from django.db import models
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
+
+from e_commerce import exceptions
+from e_commerce.decorators import raise_errors
 
 
 class BaseModel(models.Model):
@@ -27,3 +33,42 @@ def remove_none_values(obj):
         return [remove_none_values(v) for v in obj if v is not None]
     else:
         return obj
+
+
+def get_object_or_error(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        raise exceptions.Exception(
+            "%s does not exist" % model.__name__, code=status.HTTP_404_NOT_FOUND
+        )
+    except model.MultipleObjectsReturned:
+        raise exceptions.Exception(
+            "%s has multiple objects" % model.__name__,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+    except exceptions.Exception as error:
+        raise error
+
+
+def get_or_create(model, **kwargs):
+    object = model.objects.filter(**kwargs).first()
+    if object:
+        return object
+    return model.objects.create(**kwargs)
+
+
+class FileStorageAPI:
+
+    base_url = settings.FILE_STORAGE_URL
+
+    @raise_errors()
+    def _upload_file(self, file):
+        return requests.post(
+            "%s/files/" % self.base_url,
+            files={"file": file},
+        ).json()["file"]
+
+    @raise_errors()
+    def upload_file(self, *files):
+        return list(map(self._upload_file, files))
