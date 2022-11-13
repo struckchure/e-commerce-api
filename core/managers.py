6184a226b260eaf81ad524bce81582479be9340e
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-13 02:31:16 +0100

    fix(core/managers): adds product model manager

--- core/managers.py
+++ core/managers.py
@@ -3,17 +3,30 @@ from django.db import models
 
 class TagManager(models.Manager):
     def create(self, **kwargs):
-        kwargs["type"] = "TAG"
+        kwargs["type"] = self.model.META_TYPES.TAG
         return super().create(**kwargs)
 
     def get_queryset(self):
-        return super().get_queryset().filter(type="TAG")
+        return super().get_queryset().filter(type=self.model.META_TYPES.TAG)
 
 
 class CategoryManager(models.Manager):
     def create(self, **kwargs):
-        kwargs["type"] = "CATEGORY"
+        kwargs["type"] = self.model.META_TYPES.CATEGORY
         return super().create(**kwargs)
 
     def get_queryset(self):
-        return super().get_queryset().filter(type="CATEGORY")
+        return super().get_queryset().filter(type=self.model.META_TYPES.CATEGORY)
+
+
+class ProductManager(models.Manager):
+    def create(self, **kwargs):
+        tags = kwargs.pop("tags", [])
+
+        product = super().create(**kwargs)
+        product.tags.add(*tags)
+
+        return product
+
+    def get_queryset(self):
+        return super().get_queryset()
