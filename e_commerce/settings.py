83efc6700381ef6b3717c1b38d10a9f86bdf532d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-19 16:26:45 +0100

    feat(e_commerce/settings): adds drf default authentication class

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -121,4 +121,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
