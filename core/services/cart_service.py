3137dd7d9722067025792a123f4c983fee99e82d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-30 00:35:06 +0100

    style(core/services): updates service classes

--- core/services/cart_service.py
+++ core/services/cart_service.py
@@ -5,17 +5,20 @@ from e_commerce.utils import get_object_or_error, remove_none_values
 
 
 class CartService:
-    def list_items(self, user_id, skip=0, limit=10):
+    @staticmethod
+    def list_items(user_id, skip=0, limit=10):
         items = CartItem.objects.filter(user_id=user_id)[skip:limit]
 
         return CartItemSerializer(items, many=True).data
 
-    def get_item(self, item_id):
+    @staticmethod
+    def get_item(item_id):
         item = get_object_or_error(CartItem, id=item_id)
 
         return CartItemSerializer(item).data
 
-    def add_item(self, user_id, product_id, quantity):
+    @staticmethod
+    def add_item(user_id, product_id, quantity):
         item_serializer = CartItemSerializer(
             data=remove_none_values(
                 {"user": user_id, "product": product_id, "quantity": quantity}
@@ -28,7 +31,8 @@ class CartService:
 
         return item_serializer.data
 
-    def update_item(self, item_id, quantity):
+    @staticmethod
+    def update_item(item_id, quantity):
         item = get_object_or_error(CartItem, id=item_id)
         item_serializer = CartItemSerializer(
             item, data={"quantity": quantity}, partial=True
@@ -40,7 +44,8 @@ class CartService:
 
         return item_serializer.data
 
-    def delete_item(self, item_id):
+    @staticmethod
+    def delete_item(item_id):
         item = get_object_or_error(CartItem, id=item_id)
         item.delete()
 
