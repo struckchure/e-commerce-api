30b1e83fa8838f3ac1d55a67c3f83d035576ab11
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-30 14:46:30 +0100

    feat(core/models/order): adds transaction and status to order

--- core/models/order_model.py
+++ core/models/order_model.py
@@ -2,6 +2,7 @@ from e_commerce.utils import BaseModel, generate_reference
 from django.db import models
 from django.contrib.auth import get_user_model
 from core.models.product_model import Product
+from core.models.payment_model import Transaction
 
 User = get_user_model()
 
@@ -12,15 +13,14 @@ class Order(BaseModel):
         COMPLETED = "COMPLETED", "Completed"
         CANCELLED = "CANCELLED", "Cancelled"
 
-    class PAYMENT_STATUS(models.TextChoices):
-        PENDING = "PENDING", "Pending"
-        SUCCESSFUL = "SUCCESSFUL", "Successful"
-        FAILED = "FAILED", "Failed"
-
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=1)
     reference = models.CharField(max_length=50, default=generate_reference)
+    transaction = models.ForeignKey(Transaction, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(
+        max_length=50, choices=ORDER_STATUS.choices, default=ORDER_STATUS.PENDING
+    )
 
     def __str__(self):
         return f"Order {self.reference}"
@@ -30,3 +30,9 @@ class Order(BaseModel):
         if not self.product:
             return 0
         return self.product.price * self.quantity
+
+    @property
+    def payment_status(self):
+        if not self.transaction:
+            return self.ORDER_STATUS.PENDING
+        return self.transaction.status
